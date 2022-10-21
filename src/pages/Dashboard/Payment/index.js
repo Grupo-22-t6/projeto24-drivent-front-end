4ee61a2209e6979d6c17bd2ca9d4577ac801c5af
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-20 23:15:47 -0300

    feat: title and subtitle of payments page

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
+
 export default function Payment() {
   return (
-    <>
+    <Container>
       <h1>Ingresso e pagamento</h1>
-    </>
+      <h2>Primeiro, escolha sua modalidade do ingresso</h2>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  h1{
+    font-size: 34px;
+    margin-bottom: 28px;
+  }
+  h2{
+    color: #8E8E8E;
+    font-size: 20px;
+  }
+`;
