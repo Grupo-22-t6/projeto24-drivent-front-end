0eb1a3708f35398f8738624a013ba21f5b0b7531
Renan Araujo <renanaraujo852@hotmail.com>
2022-10-20 23:51:43 -0300

    feat: card button layout

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import CardButton from '../../../components/Payment/CardButton';
 
 export default function Payment() {
   return (
     <Container>
       <h1>Ingresso e pagamento</h1>
       <h2>Primeiro, escolha sua modalidade do ingresso</h2>
+      <Content>
+        <CardButton price="300">Presencial</CardButton>
+        <CardButton price="150">Online</CardButton>
+      </Content>
     </Container>
   );
 }
@@ -18,4 +23,9 @@ const Container = styled.div`
     color: #8E8E8E;
     font-size: 20px;
   }
+`;
+
+const Content = styled.div`
+display: flex;
+
 `;
