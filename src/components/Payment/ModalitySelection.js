4e01f0d075f277636317567a18259ca07c4ad885
GB-Navarro <gbnavarro@usp.br>
2022-10-23 09:32:41 -0300

    feat: added a feature that saves the ticket type selected by the user in the PaymentContext

--- src/components/Payment/ModalitySelection.js
+++ src/components/Payment/ModalitySelection.js
@@ -1,12 +1,14 @@
 import { useContext, useState } from 'react';
 import styled from 'styled-components';
 import EventInfoContext from '../../contexts/EventInfoContext';
-
+import PaymentContext from '../../contexts/PaymentContext';
 import CardButton from './CardButton';
 
 export default function ModalitySelection() {
   const { eventInfo } = useContext(EventInfoContext);
-  const [buttonStatus, setButtonStatus] = useState([false, false]);
+  const { paymentData, setPaymentData } = useContext(PaymentContext);
+  const [ buttonStatus, setButtonStatus ] = useState([false, false]);
+
   return (
     <SelectionContainer>
       {eventInfo.isPresential ? (
@@ -14,11 +16,19 @@ export default function ModalitySelection() {
           onClick={() => {
             if(buttonStatus[0] === buttonStatus[1]) {
               const newStatus = [true, false];
+              let newPaymentData = paymentData;
+              newPaymentData.isPresencial = true;
+              newPaymentData.isOnline = false;
               setButtonStatus(newStatus);
+              setPaymentData(newPaymentData);
             }
             if(buttonStatus[1] === true) {
               const newStatus = [true, false];
+              let newPaymentData = paymentData;
+              newPaymentData.isPresencial = true;
+              newPaymentData.isOnline = false;
               setButtonStatus(newStatus);
+              setPaymentData(newPaymentData);
             }
           }}
           buttonStatus={buttonStatus[0]}
@@ -33,11 +43,19 @@ export default function ModalitySelection() {
           onClick={() => {
             if(buttonStatus[0] === buttonStatus[1]) {
               const newStatus = [false, true];
+              let newPaymentData = paymentData;
+              newPaymentData.isPresencial = false;
+              newPaymentData.isOnline = true;
               setButtonStatus(newStatus);
+              setPaymentData(newPaymentData);
             }
             if(buttonStatus[0] === true) {
               const newStatus = [false, true];
+              let newPaymentData = paymentData;
+              newPaymentData.isPresencial = false;
+              newPaymentData.isOnline = true;
               setButtonStatus(newStatus);
+              setPaymentData(newPaymentData);
             }
           }}
           buttonStatus={buttonStatus[1]}
