98c54abc64798cd9a6d2b5ec7f14695e0bcb8346
GB-Navarro <gbnavarro@usp.br>
2022-10-23 09:15:59 -0300

    feat: added a props thats change the background-color of a selected ticket type

--- src/components/Payment/ModalitySelection.js
+++ src/components/Payment/ModalitySelection.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styled from 'styled-components';
 import EventInfoContext from '../../contexts/EventInfoContext';
 
@@ -6,15 +6,56 @@ import CardButton from './CardButton';
 
 export default function ModalitySelection() {
   const { eventInfo } = useContext(EventInfoContext);
-
+  const [buttonStatus, setButtonStatus] = useState([false, false]);
   return (
     <SelectionContainer>
-      {eventInfo.isPresential ? <CardButton price={eventInfo.presentialPrice/100}>Presencial</CardButton> : ''}
-      {eventInfo.isOnline ? <CardButton price={eventInfo.onlinePrice/100}>Online</CardButton> : ''}
+      {eventInfo.isPresential ? (
+        <Span
+          onClick={() => {
+            if(buttonStatus[0] === buttonStatus[1]) {
+              const newStatus = [true, false];
+              setButtonStatus(newStatus);
+            }
+            if(buttonStatus[1] === true) {
+              const newStatus = [true, false];
+              setButtonStatus(newStatus);
+            }
+          }}
+          buttonStatus={buttonStatus[0]}
+        >
+          <CardButton price={eventInfo.presentialPrice / 100}>Presencial</CardButton>
+        </Span>
+      ) : (
+        ''
+      )}
+      {eventInfo.isOnline ? (
+        <Span
+          onClick={() => {
+            if(buttonStatus[0] === buttonStatus[1]) {
+              const newStatus = [false, true];
+              setButtonStatus(newStatus);
+            }
+            if(buttonStatus[0] === true) {
+              const newStatus = [false, true];
+              setButtonStatus(newStatus);
+            }
+          }}
+          buttonStatus={buttonStatus[1]}
+        >
+          <CardButton price={eventInfo.onlinePrice / 100}>Online</CardButton>
+        </Span>
+      ) : (
+        ''
+      )}
     </SelectionContainer>
   );
 }
 
 const SelectionContainer = styled.div`
   display: flex;
+`;
+const Span = styled.span`
+  div {
+    background-color: ${(props) => props.buttonStatus ? '#FFEED2' : '#FFFFFF'};
+  }
 `;
