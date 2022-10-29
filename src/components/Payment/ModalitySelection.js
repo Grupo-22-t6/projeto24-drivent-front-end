05d920d9eae59e81f5d7919b1f9f78b41a098337
GB-Navarro <gbnavarro@usp.br>
2022-10-29 09:52:55 -0300

    feat: added some states that control the exibition of HotelSelection component and control the transition to the next page

--- src/components/Payment/ModalitySelection.js
+++ src/components/Payment/ModalitySelection.js
@@ -4,33 +4,36 @@ import EventInfoContext from '../../contexts/EventInfoContext';
 import PaymentContext from '../../contexts/PaymentContext';
 import CardButton from './CardButton';
 
-export default function ModalitySelection() {
+export default function ModalitySelection(props) {
   const { eventInfo } = useContext(EventInfoContext);
   const { paymentData, setPaymentData } = useContext(PaymentContext);
-  const [ buttonStatus, setButtonStatus ] = useState([false, false]);
-  
+  const [buttonStatus, setButtonStatus] = useState([false, false]);
+  const { setIsOnline, areOptionsSelected } = props;
+
   return (
     <SelectionContainer>
       {eventInfo.isPresential ? (
         <Span
           onClick={() => {
-            if(buttonStatus[0] === buttonStatus[1]) {
+            if (buttonStatus[0] === buttonStatus[1]) {
               const newStatus = [true, false];
               let newPaymentData = paymentData;
               newPaymentData.isPresencial = true;
               newPaymentData.isOnline = false;
-              newPaymentData.paymentValue = eventInfo.presentialPrice/100;
+              newPaymentData.paymentValue = eventInfo.presentialPrice / 100;
               setButtonStatus(newStatus);
               setPaymentData(newPaymentData);
+              setIsOnline(false);
             }
-            if(buttonStatus[1] === true) {
+            if (buttonStatus[1] === true) {
               const newStatus = [true, false];
               let newPaymentData = paymentData;
               newPaymentData.isPresencial = true;
               newPaymentData.isOnline = false;
-              newPaymentData.paymentValue = eventInfo.presentialPrice/100;
+              newPaymentData.paymentValue = eventInfo.presentialPrice / 100;
               setButtonStatus(newStatus);
               setPaymentData(newPaymentData);
+              setIsOnline(false);
             }
           }}
           buttonStatus={buttonStatus[0]}
@@ -43,23 +46,27 @@ export default function ModalitySelection() {
       {eventInfo.isOnline ? (
         <Span
           onClick={() => {
-            if(buttonStatus[0] === buttonStatus[1]) {
+            if (buttonStatus[0] === buttonStatus[1]) {
               const newStatus = [false, true];
               let newPaymentData = paymentData;
               newPaymentData.isPresencial = false;
               newPaymentData.isOnline = true;
-              newPaymentData.paymentValue = eventInfo.onlinePrice/100;
+              newPaymentData.paymentValue = eventInfo.onlinePrice / 100;
               setButtonStatus(newStatus);
               setPaymentData(newPaymentData);
+              setIsOnline(true);
+              areOptionsSelected(true);
             }
-            if(buttonStatus[0] === true) {
+            if (buttonStatus[0] === true) {
               const newStatus = [false, true];
               let newPaymentData = paymentData;
               newPaymentData.isPresencial = false;
               newPaymentData.isOnline = true;
-              newPaymentData.paymentValue = eventInfo.onlinePrice/100;
+              newPaymentData.paymentValue = eventInfo.onlinePrice / 100;
               setButtonStatus(newStatus);
               setPaymentData(newPaymentData);
+              setIsOnline(true);
+              areOptionsSelected(true);
             }
           }}
           buttonStatus={buttonStatus[1]}
@@ -78,6 +85,6 @@ const SelectionContainer = styled.div`
 `;
 const Span = styled.span`
   div {
-    background-color: ${(props) => props.buttonStatus ? '#FFEED2' : '#FFFFFF'};
+    background-color: ${(props) => (props.buttonStatus ? '#FFEED2' : '#FFFFFF')};
   }
 `;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,33 +1,70 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import HotelSelection from '../../../components/Payment/HotelSelection';
 import ModalitySelection from '../../../components/Payment/ModalitySelection';
 
 export default function Payment() {
+  const [isOnline, setIsOnline] = useState(false);
+  const [areOptionsSelected, setIfOptionsAreSelected] = useState(false);
+  const navigate = useNavigate();
+
   return (
-    <Container>
-      <h1>Ingresso e pagamento</h1>
-      <h2>Primeiro, escolha sua modalidade do ingresso</h2>
-      <ModalitySelection/>
-      <h2 className='choseAccommodation'>Ótimo! Agora escolha sua modalidade de hospedagem</h2>
-      <HotelSelection/>
-    </Container>
+    <>
+      <Container>
+        <h1>Ingresso e pagamento</h1>
+        <h2>Primeiro, escolha sua modalidade do ingresso</h2>
+        <ModalitySelection setIsOnline={setIsOnline} areOptionsSelected={setIfOptionsAreSelected} />
+        {isOnline ? (
+          <></>
+        ) : (
+          <>
+            {' '}
+            <h2 className="choseAccommodation">Ótimo! Agora escolha sua modalidade de hospedagem</h2>
+            <HotelSelection areOptionsSelected={setIfOptionsAreSelected} />
+          </>
+        )}
+      </Container>
+      <Button
+        onClick={() => {
+          if (areOptionsSelected) {
+            navigate('/dashboard/payment/card');
+          } else {
+            alert('Você precisa escolher a modalidade de ingresso antes de continuar!');
+          }
+        }}
+      >
+        RESERVAR INGRESSO
+      </Button>
+    </>
   );
 }
 
 const Container = styled.div`
-  h1{
+  h1 {
     font-size: 34px;
     margin-bottom: 28px;
   }
-  h2{
-    color: #8E8E8E;
+  h2 {
+    color: #8e8e8e;
     font-size: 20px;
   }
 
   .choseAccommodation {
     margin-top: 24px;
-    color: #8E8E8E;
+    color: #8e8e8e;
     font-size: 20px;
   }
 `;
 
+const Button = styled.button`
+  width: 180px;
+  height: 40px;
+  margin-top: 35px;
+  background: #e0e0e0;
+  border-radius: 5px;
+  border: 1px solid #e0e0e0;
+  box-shadow: 2px 2px 2px 0px rgba(224, 224, 224, 0.75);
+  -webkit-box-shadow: 2px 2px 2px 0px rgba(224, 224, 224, 0.75);
+  -moz-box-shadow: 2px 2px 2px 0px rgba(224, 224, 224, 0.75);
+`;
