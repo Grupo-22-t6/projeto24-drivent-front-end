afe150670b60758e0620126afbf69f9078aab357
GB-Navarro <gbnavarro@usp.br>
2022-10-29 09:54:05 -0300

    feat: added a prop in the styled component to control the background color of hotel options

--- src/components/Payment/HotelSelection.js
+++ src/components/Payment/HotelSelection.js
@@ -1,15 +1,60 @@
+import { useState, useContext } from 'react';
 import styled from 'styled-components';
 import CardButton from './CardButton';
+import PaymentContext from '../../contexts/PaymentContext';
 
-export default function HotelSelection() {
+export default function HotelSelection(props) {
+  const { areOptionsSelected } = props;
+  const { paymentData, setPaymentData } = useContext(PaymentContext);
+  const [buttonStatus, setButtonStatus] = useState([false, false]);
+  const newPaymentValue = paymentData.paymentValue;
+
   return (
     <SelectionContainer>
-      <CardButton price="0">Sem Hotel</CardButton>
-      <CardButton price="350">Com Hotel</CardButton> 
+      <Span
+        buttonStatus={buttonStatus[0]}
+        onClick={() => {
+          if (buttonStatus[0] === buttonStatus[1]) {
+            const newStatus = [true, false];
+            setButtonStatus(newStatus);
+            areOptionsSelected(true);
+          }
+          if (buttonStatus[1] === true) {
+            const newStatus = [true, false];
+            setButtonStatus(newStatus);
+            areOptionsSelected(true);
+          }
+        }}
+      >
+        <CardButton price="0">Sem Hotel</CardButton>
+      </Span>
+      <Span
+        buttonStatus={buttonStatus[1]}
+        onClick={() => {
+          if (buttonStatus[0] === buttonStatus[1]) {
+            const newStatus = [false, true];
+            setButtonStatus(newStatus);
+            areOptionsSelected(true);
+          }
+          if (buttonStatus[0] === true) {
+            const newStatus = [false, true];
+            setButtonStatus(newStatus);
+            areOptionsSelected(true);
+          }
+        }}
+      >
+        <CardButton price="350">Com Hotel</CardButton>
+      </Span>
     </SelectionContainer>
   );
 }
 
 const SelectionContainer = styled.div`
-  display: flex; 
+  display: flex;
+`;
+
+const Span = styled.span`
+  div {
+    background-color: ${(props) => (props.buttonStatus ? '#FFEED2' : '#FFFFFF')};
+  }
 `;
