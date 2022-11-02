6ff3216b0af4e7433c0402a3be79fde33e9eb263
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-11-02 17:17:05 -0300

    feat: get rooms vacancies api

--- src/services/hotelsApi.js
+++ src/services/hotelsApi.js
@@ -4,3 +4,8 @@ export async function getHotels() {
   const response = await api.get('/hotel');
   return response.data;
 }
+
+export async function getRoomsVacancies() {
+  const response = await api.get('/hotel/rooms');
+  return response.data;
+}
