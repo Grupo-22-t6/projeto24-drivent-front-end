4ed11d269fe455c837d8fb44dc81cfe322b8c422
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-11-02 17:16:48 -0300

    feat: get rooms vacacncies hook

--- src/hooks/api/useHotel.js
+++ src/hooks/api/useHotel.js
@@ -4,10 +4,18 @@ import * as hotelsApi from '../../services/hotelsApi';
 
 export default function useHotel() {
   const { data: hotels, loading: hotelsLoading, error: hotelsError } = useAsync(hotelsApi.getHotels);
+  const {
+    data: roomsVacancies,
+    loading: roomsVacanciesLoading,
+    error: roomsVacanciesError,
+  } = useAsync(hotelsApi.getRoomsVacancies);
 
   return {
     hotels,
     hotelsLoading,
     hotelsError,
+    roomsVacancies,
+    roomsVacanciesLoading,
+    roomsVacanciesError,
   };
 }
