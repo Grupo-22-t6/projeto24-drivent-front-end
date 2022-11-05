04fdc176550f2f9ffc4676b88021ad1803ce8f4d
Renan Araujo <renanaraujo852@hotmail.com>
2022-11-05 12:17:12 -0300

    feat: create api request get rooms function and room component

--- src/services/hotelsApi.js
+++ src/services/hotelsApi.js
@@ -4,3 +4,13 @@ export async function getHotels() {
   const response = await api.get('/hotel');
   return response.data;
 }
+
+export async function getRooms(token, hotelId) {
+  const response = await api.get(`/hotel/rooms/${hotelId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
